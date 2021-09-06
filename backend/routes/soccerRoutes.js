5fab45fe654787ceda119fa3dfc7ae6519a2b8af
Avisa-SEI <aap571420@gmail.com>
2021-09-06 12:35:03 -0700

    feat: create a PUT endpoint

--- backend/routes/soccerRoutes.js
+++ backend/routes/soccerRoutes.js
@@ -2,11 +2,12 @@ import {
   addNewPlayer,
   getPlayers,
   getPlayerById,
+  updatePlayerById,
 } from "../controllers/playerControllers";
 
 const routes = (app) => {
   app.route("/players").get(getPlayers).post(addNewPlayer);
-  app.route("/players/:playerId").get(getPlayerById);
+  app.route("/players/:playerId").get(getPlayerById).put(updatePlayerById);
 };
 
 export default routes;
