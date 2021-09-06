b8473cfecf5acc4d0daf1edf446618c45929203a
Avisa-SEI <aap571420@gmail.com>
2021-09-06 12:25:15 -0700

    feat: create specific ID GET endpoint

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import routes from "./routes/soccerRoutes";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import cors from "cors";
 const app = express();
 require("dotenv").config();
 
@@ -17,6 +18,9 @@ mongoose.connect(DATABASE_URL, {
 // bodyParser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// CORS setup
+app.use(cors());
 
 routes(app);
 
--- backend/routes/soccerRoutes.js
+++ backend/routes/soccerRoutes.js
@@ -1,7 +1,12 @@
-import { addNewPlayer, getPlayers } from "../controllers/playerControllers";
+import {
+  addNewPlayer,
+  getPlayers,
+  getPlayerById,
+} from "../controllers/playerControllers";
 
 const routes = (app) => {
   app.route("/players").get(getPlayers).post(addNewPlayer);
+  app.route("/players/:playerId").get(getPlayerById);
 };
 
 export default routes;
